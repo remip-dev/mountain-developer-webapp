c04215ac3df17affa7424905332ff0af7871b9fc
Rémi Policet <dev@remipolicet.com>
2024-09-18 12:23:08 +0200

    delete not independent test

--- src/components/__tests__/header/SiteName.spec.ts
+++ src/components/__tests__/header/SiteName.spec.ts
@@ -12,9 +12,5 @@ describe('SiteName.vue', () => {
       const wrapper = mount(SiteName)
       const siteNameElement = wrapper.find('div')
       expect(siteNameElement.classes()).toContain('site-name')
-      expect(siteNameElement.element.style.fontSize).toBe('36px')
-      expect(siteNameElement.element.style.fontWeight).toBe('700')
-      expect(siteNameElement.element.style.color).toBe('var(--tertiary-color)')
-      expect(siteNameElement.element.style.letterSpacing).toBe('30%')
     }
 })
