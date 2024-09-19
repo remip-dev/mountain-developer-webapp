f0d70a052b6083bb5c65ab2495b27c239df6e7eb
Rémi Policet <dev@remipolicet.com>
2024-09-19 16:01:27 +0200

    start config build pipeline

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,9 @@ export default mergeConfig(
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url))
+      root: fileURLToPath(new URL('./', import.meta.url)),
+      reporters: ['default', 'junit'],
+      outputFile: 'test-results.xml'
     }
   })
 )
